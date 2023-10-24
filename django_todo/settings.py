27a4760ef4dfaf3cb45ff81358e201327984343e
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-10-24 20:22:26 +0000

    Add todo app and fix key variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1wqlrjns9*x4_@*^)q)4q(&s!_#1!bzyo_@la@n0jxe%9bv=lo'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
