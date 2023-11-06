52ae2e5d1d6d86d813cd18ce00b4da6cf0124053
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-11-06 17:53:04 +0000

    Add test files and tests

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestDjango(TestCase):
+    
+    def test_this_thing_work(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_work2(self):
+            self.assertEqual(1, 1)
+
+    def test_this_thing_work3(self):
+            self.assertEqual(1, 1)
+
+    def test_this_thing_work4(self):
+            self.assertEqual(1, 1)
