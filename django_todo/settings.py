0706b38274dcdd67faa0818991c38b5436fa086e
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-11-08 10:02:28 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-debbiebergs-hellodjango-ddgatscmv4b.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-debbiebergs-hellodjango-ddgatscmv4b.ws-eu106.gitpod.io']
 
 
 # Application definition
