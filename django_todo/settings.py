c08192f4207f29d7b84b706b9a3995f627c2df57
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-11-13 14:07:42 +0000

    Update allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-debbiebergs-hellodjango-ddgatscmv4b.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['django-todo-dbe.herokuapp.com']
 
 
 # Application definition
